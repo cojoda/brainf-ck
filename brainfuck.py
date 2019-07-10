3bd6800a38324f8007dab6808bd916e933649326
Cody Danielson <cody.danielson@gmail.com>
2019-07-10 01:01:15 -0600

    Adds out of bounds detection

--- brainfuck.py
+++ brainfuck.py
@@ -1,14 +1,16 @@
 class Brainfuck:
 
-    def __init__(self, code, stream_in=''):
+    def __init__(self, code, stdin=''):
         self.code = code
         self.code_ptr = 0
         self.data = [0]
         self.data_ptr = 0
-        self.stream_in = stream_in
-        self.stream_in_ptr = 0
-        self.stream_out = []
-        self.stream_out_ptr = 0
+        self.stdin = stdin
+        self.stdin_ptr = 0
+        self.stdout = []
+        self.stdout_ptr = 0
+        self.stderr = ''
+        self.stderr_ptr = 0
 
     def match(self):
         left_count = 1
@@ -33,30 +35,36 @@ class Brainfuck:
                     self.data.append(0)
             elif instruction is '<':
                 self.data_ptr -= 1
+                if self.data_ptr < 0:
+                    print('data_ptr out of bounds')
+                    return -1
             elif instruction is ',':
-                self.data[self.data_ptr] = ord(self.stream_in[self.stream_in_ptr])
-                self.stream_in_ptr += 1
+                if self.stdin_ptr >= len(self.stdin):
+                    print('stdin_ptr out of bounds')
+                    return -1
+                self.data[self.data_ptr] = ord(self.stdin[self.stdin_ptr])
+                self.stdin_ptr += 1
             elif instruction is '.':
-                self.stream_out.append(chr(self.data[self.data_ptr]))
+                self.stdout.append(chr(self.data[self.data_ptr]))
             elif instruction is '[':
                 code_start = self.code_ptr + 1
                 code_end = self.match()
-                frame = Brainfuck(self.code[code_start:code_end], self.stream_in)
+                frame = Brainfuck(self.code[code_start:code_end], self.stdin)
                 frame.data = self.data
                 frame.data_ptr = self.data_ptr
-                frame.stream_in_ptr = self.stream_in_ptr
-                frame.stream_out = self.stream_out
-                frame.stream_out_ptr = self.stream_out_ptr
+                frame.stdin_ptr = self.stdin_ptr
+                frame.stdout = self.stdout
+                frame.stdout_ptr = self.stdout_ptr
                 while self.data[self.data_ptr] != 0:
                     frame.code_ptr = 0
                     frame.run()
                     self.data_ptr = frame.data_ptr
-                    self.stream_out_ptr = frame.stream_out_ptr
+                    self.stdout_ptr = frame.stdout_ptr
                 self.code_ptr = code_end
             self.code_ptr += 1
 
-# code = '++++++++[>++++++++<-]>[<++++>-]+<[>-<[>++++<-]>[<++++++++>-]<[>++++++++<-]+>[>++++++++++[>+++++<-]>+.-.[-]<<[-]<->] <[>>+++++++[>+++++++<-]>.+++++.[-]<<<-]] >[>++++++++[>+++++++<-]>.[-]<<-]<+++++++++++[>+++>+++++++++>+++++++++>+<<<<-]>-.>-.+++++++.+++++++++++.<.>>.++.+++++++..<-.>>-[[-]<]'
-code = '+>>++++++++++[<<[>++<-]>[<+>-]>-]<<.'
+code = '++++++++[>++++++++<-]>[<++++>-]+<[>-<[>++++<-]>[<++++++++>-]<[>++++++++<-]+>[>++++++++++[>+++++<-]>+.-.[-]<<[-]<->] <[>>+++++++[>+++++++<-]>.+++++.[-]<<<-]] >[>++++++++[>+++++++<-]>.[-]<<-]<+++++++++++[>+++>+++++++++>+++++++++>+<<<<-]>-.>-.+++++++.+++++++++++.<.>>.++.+++++++..<-.>>-[[-]<]'
+# code = '+>>++++++++++[<<[>++<-]>[<+>-]>-]<<.'
 test = Brainfuck(code)
 test.run()
-print(test.data[0])
+print(test.stdout)
